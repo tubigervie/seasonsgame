ae92875d3226222400f1d8587427b402fbfb7389
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2019-02-25 16:02:16 -0800

    started button script

---
+++ Seasons/Assets/Level Environment/Level Scripts/Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+	SpriteRenderer initial_state;
+	SpriteRenderer end_state;
+    // Start is called before the first frame update
+    void Start()
+    {
+		initial_state = this.GetComponent<SpriteRenderer>();
+		end_state = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
