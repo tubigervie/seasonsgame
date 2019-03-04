0a074f7723bc37bd9c47da658277815f0396860f
kenl5 <kenl5@uci.edu>
2019-03-03 22:28:24 -0800

    enemy AI

--- Seasons/Assets/Player/DeathTrigger.cs
+++ Seasons/Assets/Player/DeathTrigger.cs
@@ -7,6 +7,8 @@ public class DeathTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+        {
             LevelManager.singleton.StartCoroutine("DieRespawn");
+        }
     }
 }
---
+++ Seasons/Assets/Player/enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) < 5)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            speed = 0;
+            StartCoroutine(Wait());
+            LevelManager.singleton.StartCoroutine("DieRespawn");
+        }
+        if(collision.gameObject.name == "Fire")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        speed = 2;
+    }
+}
